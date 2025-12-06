86bf8ad6fc5d151a0ed6586375ee2af054b74359
User1 <NoFateTech@gmail.com>
2025-12-05 23:27:59 -0600

    basic stuff

---
+++ tests/server4/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>No NPM Laravel</title>
+    <link rel="stylesheet" href="/assets/app.css">
+</head>
+<body>
+    <h1>Hello, Blade!</h1>
+    <script src="/assets/app.js"></script>
+</body>
+</html>
